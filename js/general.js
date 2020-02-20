c7718e6807251e17c6d42dc98e48a6de5a5e764e
joseph <joseph@co7designs.com>
2020-02-20 19:06:52 +0800

    update

--- js/general.js
+++ js/general.js
@@ -18,3 +18,45 @@
     
 })(jQuery);
 
+
+
+$(document).ready(function() {
+         
+    $('#fullpage').fullpage({
+    sectionsColor: ['#000', '#eee', '#000', 'whitesmoke', '#ccddff'],
+    anchors: ['firstPage', 'secondPage', '3rdPage', '4thpage', 'lastPage'],
+    menu: '#menu',
+    scrollingSpeed: 1000
+    });
+    $(".burger-btn").mainMenu();
+       
+       $(".gal-box").click(function(){
+          var imgSel = $(this).find("img").attr("src");
+           console.log(imgSel);
+           $(".img-box").attr("src", imgSel);
+       });
+       
+       
+    });
+    
+    // init controller
+    var controller = new ScrollMagic.Controller();
+   
+    // build scene and set duration to window height
+    var scene = new ScrollMagic.Scene({triggerElement: "#trigger"})
+    .setClassToggle(".featured", "slideUp")
+    .addTo(controller);
+    
+    var scene = new ScrollMagic.Scene({triggerElement: "#trigger"})
+    .setClassToggle(".content", "slideDown")
+    .addTo(controller);
+    
+    var scene = new ScrollMagic.Scene({triggerElement: "#trigger2"})
+    .setClassToggle(".about-container", "slideLeft")
+    .addTo(controller);
+    var scene = new ScrollMagic.Scene({triggerElement: "#trigger2"})
+    .setClassToggle(".pic-container", "slideRight")
+    .addTo(controller);
+    var scene = new ScrollMagic.Scene({triggerElement: "#trigger2"})
+    .setClassToggle(".about-wel", "slideRight")
+    .addTo(controller);
